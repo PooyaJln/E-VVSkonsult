b7b1f68d7bdd05cb0823636cdda275a85ddec9bf
Pooya Jln <p.j@mailbox.org>
2023-02-02 11:01:08 +0100

    preparation before implementing sequelize ORM

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,12 +1,12 @@
-const usersDbName = 'UsersData';
-const appDbName = "heatLossApp";
+// const usersDbName = 'UsersData';
+// const appDbName = "heatLossApp";
 
-const mysqlConfig = {
-    host: "localhost",
-    user: "Pooya",
-    password: "pj8NEJ92@",
-    database: "heatLossApp"
-}
+// const mysqlConfig = {
+//     host: "localhost",
+//     user: "Pooya",
+//     password: "pj8NEJ92@",
+//     database: "heatLossApp"
+// }
 
-// module.exports = { usersDbName, appDbName }
-module.exports = { appDbName, mysqlConfig } 
+// // module.exports = { usersDbName, appDbName }
+// module.exports = { appDbName, mysqlConfig }
--- backend/connections/dbConnection.js
+++ backend/connections/dbConnection.js
@@ -1,6 +1,29 @@
 require("dotenv").config();
+
+//////////////////////// MySQL connection
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+});
+
+let poolPromise = pool.promise();
+
+pool.on("connection", function (connection, err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Connection %d acquired", connection.threadId);
+});
+
+module.exports = { pool, poolPromise };
+
+///////////////////////MongoDB connection
 // const mongoose = require("mongoose");
-const { appDbName, mysqlConfig } = require('../config/db.config')
 // const MONGO_URI = `${process.env.MONGO_URI}`;
 // const appDbURI = `${MONGO_URI}${appDbName}`;
 
@@ -11,30 +34,3 @@ const { appDbName, mysqlConfig } = require('../config/db.config')
 //     .catch(error => {
 //         console.log(error)
 //     })
-
-
-const MYSQL_DATABASE = appDbName
-const mysql = require('mysql2');
-
-
-const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT
-})
-
-// const pool = mysql.createPool(mysqlConfig)
-let poolPromise = pool.promise();
-
-pool.on('connection', function (connection, err) {
-    if (err) {
-        console.log(err);
-    }
-    console.log('Connection %d acquired', connection.threadId);
-});
-
-
-
-module.exports = { pool, poolPromise }

0a76ae005a8c52b3bece77c7c8b6b2322514a663
Pooya Jln <p.j@mailbox.org>
2023-03-29 19:05:23 +0200

    fixing merging conflict in ProjectsContext

--- frontend/src/contexts/ProjectsContext.js
+++ frontend/src/contexts/ProjectsContext.js
@@ -3,34 +3,6 @@ import { createContext, useReducer } from "react";
 import { projectsReducer } from "../reducers/projectsReducer";
 
 export const ProjectsContext = createContext();
-export const projectsReducer = (state, action) => {
-  switch (action.type) {
-    case "SET_PROJECTS":
-      return {
-        projects: action.payload,
-      };
-    case "CREATE_PROJECT":
-      return {
-        projects: [...state.projects, action.payload],
-      };
-    case "DELETE_PROJECT":
-      return {
-        projects: state.projects.filter(
-          (project) => project.project_id !== action.payload.project_id
-        ),
-      };
-    case "UPDATE_PROJECT":
-      let project = state.projects.find(
-        (project) => project.project_id === action.payload.project_id
-      );
-      Object.assign(project, action.payload);
-      return {
-        projects: state.projects,
-      };
-    default:
-      return state;
-  }
-};
 
 export const ProjectsContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(projectsReducer, {

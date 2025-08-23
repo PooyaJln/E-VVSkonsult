4526fdc5d438337ef830f0ad6b0db22e45dda0bb
PooyaJln <p.j@mailbox.org>
2025-08-23 15:11:39 +0200

    succesful typescript setup

---
+++ backend/dist/server.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv").config();
+const app = require("./app");
+const db = require("./models");
+const config = require("./config/config");
+const NODE_ENV = process.env.NODE_ENV;
+const SERVER_PORT = config.serverPort;
+const MYSQL_HOST = config.db.host;
+try {
+    (async () => {
+        await db.sequelize.sync();
+        console.log(`server.js: ${db.sequelize.config.database} is synchronized successfully.`);
+        // const app = appFnDb(db);
+        app.listen(SERVER_PORT, () => {
+            if (NODE_ENV == 'development')
+                console.log(`server started. Go to http://${MYSQL_HOST}:${SERVER_PORT}/`);
+            else
+                console.log(`server started. Go to https://${MYSQL_HOST}:${SERVER_PORT}/`);
+        });
+    })();
+}
+catch (error) {
+    console.log(`Unable to connect to the ${config.db.database} database:`);
+    console.error(error);
+}
+//# sourceMappingURL=server.js.map
